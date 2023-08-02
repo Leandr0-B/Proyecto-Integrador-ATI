e71aee4f66d454cc996775661d08812ee7364384
leandro basignani <leandrobasignani@gmail.com>
2023-08-02 00:07:47 -0300

    Fondo y favIcon

---
+++ residencial_cocoon/lib/UI/Inicio/fondoInicio.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class FondoInicio extends StatefulWidget {
+  @override
+  State<FondoInicio> createState() => _FondoInicioState();
+}
+
+class _FondoInicioState extends State<FondoInicio> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage('/images/fondo.png'),
+        fit: BoxFit.cover,
+      ),
+    )));
+  }
+}
